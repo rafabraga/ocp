01401751f1a5fbd935475f473f2306a3ba67cb67
Rafael Braga <rafael.braga@ctis.com.br>
2016-08-29 13:33:09 -0300

    Commit inicial.

---
+++ src/assertion/Principal.java
@@ -0,0 +1,16 @@
+package assertion;
+
+public class Principal {
+
+	public static void main(final String[] args) {
+		final int numero = 7;
+		testa(numero);
+	}
+
+	public static void testa(final int numero) {
+		System.out.println("vai passar");
+		assert (numero > 7) : "numero é " + numero;
+		System.out.println("passou");
+	}
+
+}
---
+++ src/exception/Principal.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class Principal {
+
+	public static void main(final String[] args) {
+		try {
+
+		} catch (StringIndexOutOfBoundsException | NullPointerException e3) {
+			// TODO: handle exception
+		}
+	}
+
+}
